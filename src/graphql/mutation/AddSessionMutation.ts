fb48573efb40082ec1e1dca90fc93795ac0eb6bc
xhxh96 <xianhao@u.nus.edu>
2021-03-02 00:19:15 +0800

    fix prettier error

--- src/graphql/mutation/AddSessionMutation.ts
+++ src/graphql/mutation/AddSessionMutation.ts
@@ -2,7 +2,9 @@ import { gql } from '@apollo/client';
 
 const ADD_SESSION_MUTATION = gql`
   mutation AddSessionMutation($name: String!, $startTime: timestamptz!, $participants: [participant_insert_input!]!) {
-    session: insert_session_one(object: { name: $name, start_time: $startTime, participants: { data: $participants } }) {
+    session: insert_session_one(
+      object: { name: $name, start_time: $startTime, participants: { data: $participants } }
+    ) {
       id
       name
     }
