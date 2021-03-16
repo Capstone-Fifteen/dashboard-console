53a4275a3e2c0ef0638368740919e2a4d7f53960
xhxh96 <xianhao@u.nus.edu>
2021-03-16 16:24:46 +0800

    fix an issue with data loader blocking background component

--- src/component/LoadingData/LoadingData.tsx
+++ src/component/LoadingData/LoadingData.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Loader } from 'rsuite';
+import './LoadingData.css';
 
 const LoadingData: React.FunctionComponent<any> = () => (
-  <div style={{ height: 300 }}>
-    <Loader size="md" center vertical content="Waiting for data..." />
+  <div className="loaderContainer">
+    <Loader vertical content="Waiting for data..." />
   </div>
 );
 
