e3d5dbe313e4fd1f7bb5360c2c384f43c5f43018
xhxh96 <xianhao@u.nus.edu>
2021-04-11 17:30:34 +0800

    fix prettier

--- src/component/DataLoader/DataLoader.tsx
+++ src/component/DataLoader/DataLoader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Loader } from 'rsuite';
 import './DataLoader.css';
 
-const DataLoader: React.FunctionComponent<any> = ({content = "Waiting for data ..."}) => (
+const DataLoader: React.FunctionComponent<any> = ({ content = 'Waiting for data ...' }) => (
   <div className="loaderContainer">
     <Loader vertical content={content} />
   </div>
